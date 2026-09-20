ee2fdd36e8a07dde2a70ef85cdb1c2ac7494f8ac
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-20 12:24:39 +0200

    add

--- nvim/lua/matugen.lua
+++ nvim/lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
   require('base16-colorscheme').setup {
     -- Background tones
-    base00 = '#131314', -- Default Background
-    base01 = '#201f20', -- Lighter Background (status bars)
-    base02 = '#43474a', -- Selection Background
-    base03 = '#c4c7c9', -- Comments, Invisibles
+    base00 = '#141314', -- Default Background
+    base01 = '#211f20', -- Lighter Background (status bars)
+    base02 = '#4a454a', -- Selection Background
+    base03 = '#cdc4ca', -- Comments, Invisibles
     -- Foreground tones
-    base04 = '#c4c7c9', -- Dark Foreground (status bars)
-    base05 = '#e5e2e2', -- Default Foreground
-    base06 = '#e5e2e2', -- Light Foreground
-    base07 = '#e5e2e2', -- Lightest Foreground
+    base04 = '#cdc4ca', -- Dark Foreground (status bars)
+    base05 = '#e6e1e2', -- Default Foreground
+    base06 = '#e6e1e2', -- Light Foreground
+    base07 = '#e6e1e2', -- Lightest Foreground
     -- Accent colors
     base08 = '#ffb4ab', -- Variables, XML Tags, Errors
-    base09 = '#cec3cd', -- Integers, Constants
-    base0A = '#c5c6c8', -- Classes, Search Background
-    base0B = '#c1c7cc', -- Strings, Diff Inserted
-    base0C = '#cec3cd', -- Regex, Escape Chars
-    base0D = '#dde3e8', -- Functions, Methods
-    base0E = '#c5c6c8', -- Keywords, Storage
+    base09 = '#d7c1c4', -- Integers, Constants
+    base0A = '#ccc4c9', -- Classes, Search Background
+    base0B = '#cfc3cd', -- Strings, Diff Inserted
+    base0C = '#d7c1c4', -- Regex, Escape Chars
+    base0D = '#ecdfe9', -- Functions, Methods
+    base0E = '#ccc4c9', -- Keywords, Storage
     base0F = '#93000a', -- Deprecated, Embedded Tags
   }
 end
